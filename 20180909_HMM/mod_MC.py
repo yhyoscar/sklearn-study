7e68b215c6f06773a0cf68fb14c1a54d6367efe2
oscar <yhyoscar@gmail.com>
2018-09-09 18:35:54 -0400

    update HMM

--- 20180909_HMM/mod_MC.py
+++ 20180909_HMM/mod_MC.py
@@ -72,4 +72,63 @@ def HMM_simulation(A, B, nt=100, pxinit=None, xstr=None, ystr=None):
 
     return x, y
 
+# Baum-Welch algorithm
+# A: [nx, nx] transition matrix a(i,j) = P(x(t+1)=j|x(t)=i)
+# B: [nx, ny] emission matrix b(i,j) = P(y(t)=j|x(t)=i)
+# y: [nt] observation sequence of index [0,1,...,ny-1]
+# pxinit: [nx] initial probability distribution of hiddens states
+def baum_welch(y, nx=2, pxinit=None, maxiter=100):
+    ny = len(set(y))
+    nt = len(y)
+    if pxinit is None: pxinit = np.array([1.0/nx for i in range(nx)])
+
+    A = np.random.uniform(size=nx*nx).reshape([nx,nx])
+    A /= np.sum(A, axis=1).reshape([nx,1])
+    B = np.random.uniform(size=nx*ny).reshape([nx,ny])
+    B /= np.sum(B, axis=1).reshape([nx,1])
+    
+    error = 999
+    k = 0
+    alpha = np.zeros([nt, nx])
+    beta  = np.zeros([nt, nx])
+    errors = []
+    
+    while (k<maxiter and error>1e-6):
+        # forward
+        alpha[0,:] = pxinit * B[:,y[0]]
+        for t in range(1,nt):
+            alpha[t,:] = np.dot(alpha[t-1,:], A) * B[:,y[t]]
+        
+        # backward
+        beta[nt-1,:] = 1.0
+        for t in range(nt-1)[::-1]:
+            beta[t,:] = np.dot(A, beta[t+1,:] * B[:,y[t+1]])
+
+        #print(alpha.T)
+        #print(beta.T)
+            
+        # update
+        px = alpha * beta / np.sum(alpha*beta, axis=1).reshape([nt, 1])
+        p2d = np.zeros([nt, nx, nx])
+        for t in range(nt-1):
+            p2d[t,:,:] = alpha[t,:].reshape([nx,1]) * A * \
+                beta[t+1,:].reshape([1,nx]) * B[:, y[t+1]].reshape([1,nx])
+            p2d[t,:,:] /= np.sum(p2d[t,:,:])
+        
+        pxinit = px[0,:]
+        Anew = np.sum(p2d[:nt-1,:,:], axis=0) / np.sum(px[:nt-1,:], axis=0).reshape([nx,1])
+        Bnew = np.zeros([nx, ny])
+        for t in range(nt-1):
+            Bnew[:, y[t]] += px[t,:]
+        Bnew /= np.sum(px, axis=0).reshape([nx, 1])
+        
+        error = np.mean((Anew-A)**2) + np.mean((Bnew-B)**2) 
+        errors.append(error)
+        A = Anew + 0
+        B = Bnew + 0
+        k += 1
+        
+        #print(k, error)
+        
+    return A, B, errors
 
